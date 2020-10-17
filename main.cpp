c9aebeec5d746b1f5b0129578b7ed622cd9b62d3
Mistes <hernan.pereira@udea.edu.co>
2020-10-17 15:27:25 -0500

    Desarrollo

--- main.cpp
+++ main.cpp
@@ -7,7 +7,17 @@ int main()
     int defx=0, defy=0;
     int atax=0, atay=0;
     int vel_at, vel_def;
+    int distancia=0;
+    int puntomedio;
+    int tiempo;
 
+    cout<<"Distancia entre ambos frentes (m): ";cin>>distancia;
+    cout<<"Velocidad de la bala de ataque (m/s): ";cin>>vel_at;
+    cout<<"Velocidad de la bala de defensa (m/s): ";cin>>vel_def;
+
+    puntomedio=distancia/2;
+
+    tiempo=puntomedio/vel_at;
 
 
 
